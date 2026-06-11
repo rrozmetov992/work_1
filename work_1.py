35af1fc3266c0fbdad83134df9b31f7fb1ff7d35
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:04:50 +0300

    Add files via upload

---
+++ work_1.py
@@ -0,0 +1,34 @@
+#1
+#a=int(input("a="))
+#print("Kvadratning perimetri=", 4*a)
+#2
+# a=int(input("a="))
+# print("Kvadratning yuzasi=", a*a)
+#3
+# a=int(input("a="))
+# b=int(input("b="))
+# print("to'rtburchakning yuzi=", a*b)
+# print("to'rtburchakning perimetri=", 2*(a+b))
+#4
+# import math
+# d=int(input("d="))
+# print("aylananing uzunligi=", math.pi*d)
+#5
+# a=int(input("a="))
+# print("Kubning hajmi=", a**3)
+# print("kubning sirt yuzi=", 6*a**2)
+#6
+# a=int(input("a="))
+# b=int(input("b="))
+# c=int(input("c="))
+# print("Paralelopipet hajmi=", a*b*c)
+# print("Paralelopipet Sirt yuzi=", 2*(a*b+b*c+a*c))
+#7
+# import math
+# r=int(input("r="))
+# print("Doiraning uzunligi=", 2*math.pi*r)
+# print("doiraning yuzi=", math.pi*r**2)
+#8
+# a=int(input("a="))
+# b=int(input("b="))
+# print("ikkita sonning o'rta arifmetigi=", (a+b)/2)
